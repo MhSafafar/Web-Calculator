ee2a4bde9b9c830f945a6feec6f65b10ac1945c0
Ashkan Partovi <partovi.code@gmail.com>
2025-11-18 18:50:50 +0330

    feat_add: the "mod" button is functional now

--- src/script.js
+++ src/script.js
@@ -62,6 +62,8 @@ function inputValidation(eval_str) {
     eval_str = eval_str.replace(/([\d.]+)Math/g, '$1*Math.sqrt(');
     // Replace "log" and with "Math.log10("
     eval_str = eval_str.replace(/log/g, "Math.log10(");
+    // Replace "mod" and with "%"
+    eval_str = eval_str.replace(/mod/g, "%");
     // Replace factorial (simple integer case)
     eval_str = eval_str.replace(/(\d+)!/g, 'factorial($1)');
     // Auto close prantheses
